2ed1cd8b23d42cc7d294052087057d3b56df19c1
Eric Deutsch <edeutsch@systemsbiology.org>
2021-10-10 23:52:38 -0700

    add some files to get started

---
+++ scripts/read_fasta.py
@@ -0,0 +1,19 @@
+#!/bin/env python3
+
+import re
+from Bio import SeqIO
+
+filename = '../proteomes/maize/original/mitochondrion.2.fasta'
+with open(filename) as infile:
+
+    print(f"INFO: Reading {filename}")
+    for record in SeqIO.parse(infile, 'fasta'):
+        sequence = str(record.seq)
+        match = re.match(r'^(.+?)\s+(.*)$', record.description)
+        if match:
+            identifier = match.group(1)
+            description = match.group(2)
+        else:
+            print(f"ERROR: Unable to parse description line: {record.description}")
+            exit()
+        print(f"{identifier} - {description}")
