3ca7e778e01bea44ae3c24e424b995a232079db6
Eric Deutsch <edeutsch@systemsbiology.org>
2021-12-09 17:01:46 -0800

    add new pull file for human files

---
+++ proteomes/human/original/get_data.sh
@@ -0,0 +1,6 @@
+
+
+curl -O -R ftp://ftp.peptideatlas.org/pub/PeptideAtlas/Proteomes/human/PhaseI_ORFs_20210405.fasta
+
+curl -O -R ftp://ftp.peptideatlas.org/pub/PeptideAtlas/Proteomes/human/PA_THISP_Level4_2021-03-01_targetdecoy.fasta
+
