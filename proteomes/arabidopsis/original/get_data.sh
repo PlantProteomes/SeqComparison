6b1a327f5b4d3cf44e294367a984d49e3e131d4c
Eric Deutsch <edeutsch@systemsbiology.org>
2022-01-10 16:51:57 -0800

    Added curl commands to download all the Arabidopsis sequence databases

---
+++ proteomes/arabidopsis/original/get_data.sh
@@ -0,0 +1,19 @@
+
+
+curl -O -R ftp://ftp.peptideatlas.org/pub/PeptideAtlas/Proteomes/arabidopsis/Araport11.fasta
+
+curl -O -R ftp://ftp.peptideatlas.org/pub/PeptideAtlas/Proteomes/arabidopsis/TAIR10.fasta
+
+curl -O -R ftp://ftp.peptideatlas.org/pub/PeptideAtlas/Proteomes/arabidopsis/UniProtKB.fasta
+
+curl -O -R ftp://ftp.peptideatlas.org/pub/PeptideAtlas/Proteomes/arabidopsis/RefSeq.fasta
+
+curl -O -R ftp://ftp.peptideatlas.org/pub/PeptideAtlas/Proteomes/arabidopsis/ARA-PEP-LW.fasta
+
+curl -O -R ftp://ftp.peptideatlas.org/pub/PeptideAtlas/Proteomes/arabidopsis/ARA-PEP-SIPs.fasta
+
+curl -O -R ftp://ftp.peptideatlas.org/pub/PeptideAtlas/Proteomes/arabidopsis/ARA-PEP-sORFs.fasta
+
+curl -O -R ftp://ftp.peptideatlas.org/pub/PeptideAtlas/Proteomes/arabidopsis/IowaORFs.fasta
+
+
