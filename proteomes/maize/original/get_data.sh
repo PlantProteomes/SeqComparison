f5f61664e275c48b102974d85e02525e41cd57c5
Eric Deutsch <edeutsch@systemsbiology.org>
2022-01-26 11:03:29 -0800

    added 3 more databases

--- proteomes/maize/original/get_data.sh
+++ proteomes/maize/original/get_data.sh
@@ -14,3 +14,9 @@ curl -O -R ftp://ftp.peptideatlas.org/pub/PeptideAtlas/Proteomes/maize/uniprot-p
 
 curl -O -R ftp://ftp.peptideatlas.org/pub/PeptideAtlas/Proteomes/maize/ProFITS_TF_4a53_protein.2.remove.fasta
 
+curl -O -R ftp://ftp.peptideatlas.org/pub/PeptideAtlas/Proteomes/maize/agriGO_151.2.remove.fasta
+
+curl -O -R ftp://ftp.peptideatlas.org/pub/PeptideAtlas/Proteomes/maize/Zea_mays.AGPv3.22.pep.all.remove.2.fa
+
+curl -O -R ftp://ftp.peptideatlas.org/pub/PeptideAtlas/Proteomes/maize/Zm-B73-REFERENCE-GRAMENE-4.0_Zm00001d.2.protein.2.fa
+
