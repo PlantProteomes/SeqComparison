1498fd15f40126496d18706cd21fe6b9b84a3408
MLi1104 <jli@eastsideprep.org>
2022-08-08 13:50:13 -0700

    more efficient table generation

--- scripts/ptm_mc_analysis_ML.py
+++ scripts/ptm_mc_analysis_ML.py
@@ -7,76 +7,109 @@
 import pandas as pd
 import os.path
 
-# if specified file no exist, make the file. Else skip to dataframe
-if os.path.exists("C:\\Users\\jli\\plantproteomes\\SeqComparison\\proteomes\\arabidopsis\\phospho_atmg.txt") == False:
-    with open("C:\\Users\\jli\\plantproteomes\\SeqComparison\\proteomes\\arabidopsis\\protein_PTM_summary_STY_Phospho.txt") as ptm_file:
-        lines = ptm_file.readlines()
-
-    at_file = open("C:\\Users\\jli\\plantproteomes\\SeqComparison\\proteomes\\arabidopsis\\phospho_atmg.txt", "w")
-    at_file.write(lines[0])
-
-    for line in lines:
-        split = line.split("\t")
-        identifier = split[0]
-        if identifier.startswith("PeptideAtlas_ATMG") and identifier.endswith(".1"):
-            at_file.write(line)
-
-    at_file.close()
-    print("All ATMG entries written to file!")
-
-
-## Get Statistics ##
-
-# dataframe with only at*g proteins
-df = pd.read_csv(
-    "C:\\Users\\jli\\plantproteomes\\SeqComparison\\proteomes\\arabidopsis\\phospho_atmg.txt", sep="\t")
-
-print("Statistics:")
-
-# get distinct identifiers
-distincts = df["protein"].unique()
-print(f"Total number of distinct identifiers: {len(distincts)}")
-# f = open("atmg_distincts.txt", "w")
-# for distinct in distincts:
-    # f.write(distinct + "\n")
-# f.close()
-print("txt file created!")
-
-# get total number of potential sites
-total_sites = df.shape[0]
-print(f"Total number of potential sites: {total_sites}")
-
-# get total sites with at least 1 observation
-df_1p = df[df["nObs"] > 0]
-sites_1p = df_1p.shape[0]
-print(f"Total sites with at least 1 observation: {sites_1p}")
-
-# select sites with nP99 and above
-df_above = df[(df["nP99"] > 0) | (df["nP100"] > 0) | (df["no-choice"] > 0)]
-print(f"Sites with nP99 or above: {df_above.shape[0]}")
-
-# select distinct sites with nP99 and above
-n_dist_above = len(df_above["protein"].unique())
-print(f"Distinct sites with nP99 or above: {n_dist_above}")
-
-# assemble new dataframe for stats on this ptm/at*g
-column = [len(distincts), total_sites, sites_1p, df_above.shape[0], n_dist_above]
-df_sum = pd.DataFrame()
-df_sum["Mitochondria"] = column
-
-
-## Writing to Excel##
-
-# writer for probability table
-writer1 = pd.ExcelWriter(
-    "C:\\Users\\jli\\plantproteomes\\SeqComparison\\proteomes\\arabidopsis\\ptm_analysis.xlsx")
-df_above.to_excel(writer1, "atmg")
-writer1.save()
-print('Dataframe is written successfully to Excel File.')
-
-# writer for generating the c or m column for the stats table
-writer2 = pd.ExcelWriter(
+keywords = ["protein_PTM_summary_K_GG", "gg"]
+df_summary = pd.DataFrame()
+analysis_writer = pd.ExcelWriter(
+        "C:\\Users\\jli\\plantproteomes\\SeqComparison\\proteomes\\arabidopsis\\ptm_analysis.xlsx")
+summary_writer = pd.ExcelWriter(
     "C:\\Users\\jli\\plantproteomes\\SeqComparison\\proteomes\\arabidopsis\\ptm_stats.xlsx")
-df_sum.to_excel(writer2, "atmg")
-writer2.save()
-print('Dataframe is written successfully to Excel File.')
+
+# get statistics on a ptm file for atcg, atmg, and nuclear
+for i in range (0, 3):
+    # establish what data we are looking for, atcg, atmg, or nuclear
+    if i == 0:
+        data_type = "atcg"
+        col_name = "Chloroplasts"
+    elif i == 1:
+        data_type = "atmg"
+        col_name = "Mitochondria"
+    elif i == 2:
+        data_type = "nuclear"
+        col_name = "Nuclear"
+
+    # if specified file no exist, make the file. Else skip to dataframe
+    if os.path.exists(f"C:\\Users\\jli\\plantproteomes\\SeqComparison\\proteomes\\arabidopsis\\{keywords[1]}_{data_type}.txt") == False:
+        with open(f"C:\\Users\\jli\\plantproteomes\\SeqComparison\\proteomes\\arabidopsis\\{keywords[0]}.txt") as ptm_file:
+            lines = ptm_file.readlines()
+
+        at_file = open("C:\\Users\\jli\\plantproteomes\\SeqComparison\\proteomes\\arabidopsis\\phospho_nuclear.txt", "w")
+        at_file.write(lines[0])
+    
+        if data_type == "nuclear":
+            for line in lines:
+                split = line.split("\t")
+                identifier = split[0]
+                if (identifier.startswith("AT1G") or
+                    identifier.startswith("AT2G") or
+                    identifier.startswith("AT3G") or
+                    identifier.startswith("AT4G") or
+                    identifier.startswith("AT5G")):
+                    if identifier.endswith(".1"):
+                        at_file.write(line)
+
+        if data_type == "atcg" or data_type == "atmg":
+            for line in lines:
+                split = line.split("\t")
+                identifier = split[0]
+                if (identifier.startswith(data_type.upper()) and 
+                    identifier.endswith(".1")):
+                        at_file.write(line)
+
+        at_file.close()
+        print("Entries written to file!")
+
+
+    ## Get Statistics ##
+
+    # dataframe with only at*g proteins
+    df = pd.read_csv(
+        f"C:\\Users\\jli\\plantproteomes\\SeqComparison\\proteomes\\arabidopsis\\{keywords[1]}_{data_type}.txt", 
+        sep="\t", index_col = False)
+
+    print(f"{data_type} statistics:")
+
+    # get distinct identifiers
+    distincts = df["protein"].unique()
+    print(f"Total number of distinct identifiers: {len(distincts)}")
+    # f = open("atmg_distincts.txt", "w")
+    # for distinct in distincts:
+        # f.write(distinct + "\n")
+    # f.close()
+    # print("txt file created!")
+
+    # get total number of potential sites
+    total_sites = df.shape[0]
+    print(f"Total number of potential sites: {total_sites}")
+
+    # get total sites with at least 1 observation
+    df_1p = df[df["nObs"] > 0]
+    sites_1p = df_1p.shape[0]
+    print(f"Total sites with at least 1 observation: {sites_1p}")
+
+    # select sites with nP99 and above
+    df_above = df[(df["nP99"] > 0) | (df["nP100"] > 0) | (df["no-choice"] > 0)]
+    df_above = df_above.reset_index(drop = True)
+    print(f"Sites with nP99 or above: {df_above.shape[0]}")
+
+    # select distinct sites with nP99 and above
+    n_dist_above = len(df_above["protein"].unique())
+    print(f"Distinct sites with nP99 or above: {n_dist_above}")
+
+     # total sum of confident psms
+    n_psms = df["nP99"].sum() + df["nP100"].sum() + df["no-choice"].sum()
+    print(f"Total PSMs for sites with nP99 or above: {n_psms}")
+
+    # assemble new dataframe for stats on this ptm/at*g
+    column = [len(distincts), total_sites, sites_1p, df_above.shape[0], n_dist_above, n_psms]
+    df_summary[col_name] = column
+
+    # writer for probability table
+    df_above.to_excel(analysis_writer, data_type)
+    analysis_writer.save()
+    print(f'{data_type} dataframe is written successfully to Excel File.')
+    print("")
+
+# write stats summary to excel
+df_summary.to_excel(summary_writer)
+summary_writer.save()
+print('Summary dataframe is written successfully to Excel File.')
